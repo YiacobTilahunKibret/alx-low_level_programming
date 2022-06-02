c1238bbcea04944e2c132f9f612f5b615a45f6dd
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-06-02 14:09:10 -0700

    printing out using put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * The Program prints out programming is like 
+ * building a multilingual puzzle
+ */
+int main ()
+{
+    char str[] = "Programming is like building a multilingual puzzle";
+    puts (str);
+    return 0;
+}
