5bbec660c6c5f79457edfe8093c3408ae60ec707
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-06-02 15:56:45 -0700

    betty is very dangerous

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * The Program prints out programming is like 
- * building a multilingual puzzle
+ * prints the phrase "\"Programming is like building a multilingual puzzle"
+ * Return: 0 if exited properly, non-zero otherwise
  */
+
 int main(void)
 {
-	char str[] = ""Programming is like building a multilingual puzzle";
-	puts(str);
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
