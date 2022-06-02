640dd1b749d8a285f9ffd502d5e975f078b80f5c
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-06-02 16:44:36 -0700

    sizeof characters

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,12 +3,12 @@
 /**
  * main - prints out size of characters
  * Description - uses printf and sizeof
- * Return - 0
+ * Return: 0 (always)
  */
 
 int main(void)
 {
-	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
