6b220e256771fd0523480d5b97e3cddf6d3946e5
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-06-02 18:12:10 -0700

    simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Description: uses unistd.h lib
+ * Return: 0
+ */
+
+int main(void)
+{
+	write(2, "\nand that piece of art is useful\" - Dora Korpar,2015-10-19", 59);
+	return(1);
+}
