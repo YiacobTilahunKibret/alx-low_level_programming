7cdca1e085b41810ada6f00629d0535821380fd1
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-06-03 19:13:47 -0700

    prints alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ * Description: prints alphabet (lowercase then uppercase)
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
