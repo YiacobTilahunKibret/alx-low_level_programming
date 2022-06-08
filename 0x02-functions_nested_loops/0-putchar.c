e68f8b7976e2da9a1d5ee3dd7cffe6e0e8946ee7
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-06-08 12:40:48 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: prints  _putchar using local library
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	return (0);
+}
