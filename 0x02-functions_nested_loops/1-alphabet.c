9a823f3bd511afe3820dd488f2383c1af548af3f
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-06-08 13:47:31 -0700

    print a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+/**
+ * print_alphabet - entry point
+ * Description: prints  _ the alphabet in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
