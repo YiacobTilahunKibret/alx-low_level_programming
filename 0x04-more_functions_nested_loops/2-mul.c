b7e2cd07ecbb8b2f191d32ff838682cba738d58a
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-06-09 13:50:03 -0700

    return multiple of two values

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - gives the product a and b
+ * @a: variable passed
+ * @b: variable passed
+ * Return: multiple of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
