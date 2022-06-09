c1663a0761e4bc407d3ccf0525c94dad74f67c0a
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-06-09 14:33:19 -0700

    prints 0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers -,prints 0 to 14
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	char i, j;
+	int k = 0;
+
+	whike(k < 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			j = i;
+			if (i > 9)
+			{
+				_putchar('1');
+				j = i % 10;
+			}
+			_putchar('0' + c);
+		}
+		_putchar('\n');
+		k++;
+	}
+}
