6a909f189dc78eabbae7c9d35bcdb41a598dd1e6
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-06-08 17:05:45 -0700

    absloute number of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * Description: retuns absloute value of a number
+ * @n: parameter declared
+ * Return: absolute value of a number
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
