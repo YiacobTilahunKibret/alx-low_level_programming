2ae180473cf71b5008fd60025ba3b6cae2bc3189
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-06-13 16:50:58 -0700

    hot fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,20 +9,17 @@
  * Return: the pointer to dest
  */
 
+
 char *_strcpy(char *dest, char *src)
 {
-	int i, length;
+	int i = -1;
 
-	while (src[length] != '\0')
-	{
-		length++;
-	}
-
-	for (i = 0; i < length; i++)
-	{
+	do {
+		i++;
 		dest[i] = src[i];
 	}
-	dest[i] = '\0';
+
+	while (src[i] != '\0');
 
 	return (dest);
 }
