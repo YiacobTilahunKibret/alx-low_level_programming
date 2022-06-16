6bb60d8da370a150733aa0b64b3d3bbc2d2e689e
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-06-16 08:13:21 -0700

    change to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper  - entry point
+ * Description: function that changes all
+ * lowercase letters of a string to uppercase
+ * @s: string parameter passed
+ * Return: uppercase string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
