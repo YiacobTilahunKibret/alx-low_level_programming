ae7f047e332ffd6d67c546121f46a8895217ff93
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-06-24 16:33:16 -0700

    hot fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,3 +14,17 @@ int find_root(int n, int i)
 	else
 		return (-1);
 }
+/**
+ * _sqrt_recursion - calculates the natural square root of n.
+ * @n: base number.
+ *
+ * Return: the square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (find_root(n, 2));
+}
