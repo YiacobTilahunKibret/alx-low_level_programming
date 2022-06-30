db74641f8cddda610c0480652b20b4f1e447042f
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-06-30 14:21:29 -0700

    hot fix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -12,7 +13,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int is_digit(char *s);
 int _strlen(char *s);
 void errors(void);
-int _putchar(char c);
-
 
 #endif/*MAIN_H*/
