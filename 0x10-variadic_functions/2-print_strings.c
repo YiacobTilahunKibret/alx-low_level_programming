f9bdba8213b22f2baff4ee7e03bfaaebd51ec325
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-07-07 11:48:04 -0700

    hot fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+
 /**
  * print_strings - Print numbers using a separator
  * @separator: The string separator
@@ -9,19 +10,19 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int i;
-	char *p;
+va_list valist;
+unsigned int i;
+char *p;
 
-	va_start(valist, n);
+va_start(valist, n);
 
-	for (i = 0; i < n; i++)
-	{
-		if (separator != NULL && i != 0)
-			printf("%s", separator);
-		p = va_arg(valist, char *);
-		printf("%s", (p == NULL) ? "(nil)" : p);
-	}
-	printf("\n");
-	va_end(valist);
+for (i = 0; i < n; i++)
+{
+if (separator != NULL && i != 0)
+printf("%s", separator);
+p = va_arg(valist, char *);
+printf("%s", (p == NULL) ? "(nil)" : p);
+}
+printf("\n");
+va_end(valist);
 }
