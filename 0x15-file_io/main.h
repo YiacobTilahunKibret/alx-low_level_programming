12691c021bd2735d05b02f97416bda901f9efb98
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2022-07-25 11:51:17 -0700

    hot fix

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,6 +12,10 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+/**
+ * check_elf - ELF header functions
+ * @e_ident: passed array
+*/
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
@@ -22,5 +26,4 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
-
 #endif
